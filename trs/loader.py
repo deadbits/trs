deac0b9e58c4f9e74a84f498ffb276760420a907
Adam Swanda <adam@deadbits.org>
2023-10-15 19:35:12 -0400

    PDF to document loader

--- trs/loader.py
+++ trs/loader.py
@@ -21,6 +21,36 @@ class Loader:
             return None
 
         content = '\n'.join([elem.text for elem in elements])
-        doc = Document(source=url, text=content)
+        doc = Document(
+            source=url,
+            text=content,
+            metadata={'type': 'url'}
+        )
         return doc   
 
+    def pdf(self, file_path: str) -> Document:
+        """Parse a PDF file to text and return a documents"""
+
+        if not os.path.exists(file_path):
+            logger.error(f'file {file_path} does not exist')
+            return None
+
+        texts = []
+        with open(file_path, 'rb') as fp:
+            logger.info(f'loading pdf: {file_path}')
+
+            pdf_reader = PyPDF2.PdfReader(fp)
+            num_pages = len(pdf_reader.pages)
+
+            for page_num in range(num_pages):
+                # this could be done better by retaining the page number
+                # from the pdf so it could be used for citations
+                page = pdf_reader.pages[page_num]
+                texts.append(page.extract_text())
+
+        doc = Document(
+            source=file_path,
+            text='\n'.join(texts),
+            metadata={'type': 'pdf'}
+        )
+        return docs
