77962e4227f93875bbe1abe4c757d0ce061bbd2b
Adam Swanda <adam@deadbits.org>
2023-10-16 19:10:18 -0400

    generic variable names for loaders

--- trs/loader.py
+++ trs/loader.py
@@ -10,34 +10,34 @@ from .schema import Document
 
 
 class Loader:
-    def url(self, url: str) -> Document:
+    def url(self, source: str) -> Document:
         """Retrieve a URL and return a Document containing the text"""
-        logger.info(f'loading url: {url}')
+        logger.info(f'loading url: {source}')
 
         try:
-            elements = partition_html(url=url)
+            elements = partition_html(url=source)
         except Exception as err:
-            logger.error(f'error retrieving html: {url} - {err}')
+            logger.error(f'error retrieving html: {source} - {err}')
             return None
 
         content = '\n'.join([elem.text for elem in elements])
         doc = Document(
-            source=url,
+            source=source,
             text=content,
             metadata={'type': 'url'}
         )
         return doc   
 
-    def pdf(self, file_path: str) -> Document:
+    def pdf(self, source: str) -> Document:
         """Parse a PDF file to text and return a documents"""
 
-        if not os.path.exists(file_path):
-            logger.error(f'file {file_path} does not exist')
+        if not os.path.exists(source):
+            logger.error(f'file {source} does not exist')
             return None
 
         texts = []
-        with open(file_path, 'rb') as fp:
-            logger.info(f'loading pdf: {file_path}')
+        with open(source, 'rb') as fp:
+            logger.info(f'loading pdf: {source}')
 
             pdf_reader = PyPDF2.PdfReader(fp)
             num_pages = len(pdf_reader.pages)
@@ -49,7 +49,7 @@ class Loader:
                 texts.append(page.extract_text())
 
         doc = Document(
-            source=file_path,
+            source=source,
             text='\n'.join(texts),
             metadata={'type': 'pdf'}
         )
